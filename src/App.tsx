a9bf39a96dc17e823d6b235b01fac3292e6c2605
Angel Herrera <angelherrerax7@gmail.com>
2026-03-04 22:40:28 -0500

    fix: remove testimonial section

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Experience from './components/Experience';
 import Skills from './components/Skills';
-import Testimonial from './components/Testimonial';
 import Footer from './components/Footer';
 import './index.css';
 
@@ -14,7 +13,6 @@ function App() {
         <Hero />
         <Experience />
         <Skills />
-        <Testimonial />
         <Footer />
       </main>
     </>
