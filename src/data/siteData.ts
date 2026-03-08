cc7a51a57f82779106f43d525cf2c446dd77fd8b
Angel Herrera <angelherrerax7@gmail.com>
2026-03-07 19:42:00 -0500

    chore(data): delete unused siteData.ts barrel

--- src/data/siteData.ts
+++
@@ -1,6 +0,0 @@
-export { siteOwner, socialLinks, careerStart, isOpenForOpportunities } from './owner';
-export { navLinks } from './navigation';
-export { heroContent } from './hero';
-export { experienceTimeline } from './experience';
-export { skillCategories } from './skills';
-export { contactContent } from './contact';
